db066d145fad2e03679ba4bbf6205b566af3d3b2
Zachary Wu <zawu@ucsd.edu>
2014-02-13 09:51:39 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -13,8 +13,32 @@ function initializePage() {
 
 	$('#colorBtn').click(randomizeColors);
 }
+var idNumber;
+$("p").click(function(e)
+{
+	var projectID = $(this).closest('.project').attr('id');
+	idNumber = projectID.substr('project'.length);
+	console.log("/project/" + idNumber);
+	$.get("/project/" + idNumber, addProject);
+})
 
-/*
+function addProject(result){
+	console.log(result);
+	var projectHTML = '<a href="#" class ="thumbnail">' + 
+	'<img src="' + result['image'] + '" class="detailsImage">' +
+	 '<p>' + result['title'] + '</p>' + '<p><small>' +
+	  result['date'] + '</small></p></a>' + result['summary'];
+	//var projectID = $(this).closest('.project').attr('id');
+	//var idNumber = projectID.substr('project'.length);
+	//$(""#project"+idNumber > ".details"").html("FOO");
+	// var test = '#project'+idNumber + ' > details';
+	// console.log(test);
+	// $("#project"+idNumber + " details").html("FOO");
+	// $(test).html("FOO");	
+	$('#project'+idNumber+ ' .details').html(projectHTML);	
+	//$("#project-description").html(result['summary']);
+}
+/*	
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
@@ -35,4 +59,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", addColor);
+}
+
+function addColor(result){
+	var temp = result['colors'];
+	var colors = temp['hex'];
+	console.log(colors);
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
